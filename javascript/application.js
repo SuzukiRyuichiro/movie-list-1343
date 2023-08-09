641f1271c4d6e9aa748baee0a3b4ff55362078f4
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-09 12:58:00 +0100

    no mustache

--- javascript/application.js
+++ javascript/application.js
@@ -1,5 +1,3 @@
-import Mustache from "mustachejs";
-
 const url = "http://www.omdbapi.com/?i=tt3896198&apikey=2b73eb94&s=harry";
 const results = document.querySelector("#results");
 const template = document.querySelector("#movieCardTemplate");
