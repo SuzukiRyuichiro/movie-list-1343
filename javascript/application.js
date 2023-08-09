9f1d66aa374847864e20e942821e6599c6ee8c61
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-09 10:44:54 +0100

    mustache

--- javascript/application.js
+++ javascript/application.js
@@ -7,7 +7,12 @@ const template = document.querySelector("#movieCardTemplate").innerHTML;
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    const moviesData = { movies: data.Search };
-    const movieCards = Mustache.render(template, moviesData);
-    results.insertAdjacentHTML("beforeend", movieCards);
+    const movies = data.Search;
+    movies.forEach((movie) => {
+      // Solution 3 (mustaches)
+      // Use mustache to interpolate the title, year, etc
+      const card = Mustache.render(template, movie);
+      // Insert that generated card into the #results
+      results.insertAdjacentHTML("beforeend", card);
+    });
   });
