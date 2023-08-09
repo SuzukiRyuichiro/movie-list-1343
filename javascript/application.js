5361df83f2ca41573177c35cb25f2f1a94656387
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-09 10:47:15 +0100

    html template

--- javascript/application.js
+++ javascript/application.js
@@ -2,17 +2,23 @@ import Mustache from "mustachejs";
 
 const url = "http://www.omdbapi.com/?i=tt3896198&apikey=2b73eb94&s=harry";
 const results = document.querySelector("#results");
-const template = document.querySelector("#movieCardTemplate").innerHTML;
+const template = document.querySelector("#movieCardTemplate");
 
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
     const movies = data.Search;
     movies.forEach((movie) => {
-      // Solution 3 (mustaches)
-      // Use mustache to interpolate the title, year, etc
-      const card = Mustache.render(template, movie);
-      // Insert that generated card into the #results
-      results.insertAdjacentHTML("beforeend", card);
+      // Solution 2 (clone the template)
+      const clone = template.content.cloneNode(true);
+      // modify the content of that clone
+      clone.querySelector("img").src = movie.Poster;
+      clone.querySelector("h6.card-title").innerText = movie.Title;
+      clone.querySelector("p.card-text").innerText = movie.Year;
+      clone.querySelector(
+        "a"
+      ).href = `https://www.imdb.com/title/${movie.imdbID}`;
+      // insert that into the #results
+      results.appendChild(clone);
     });
   });
